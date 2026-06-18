5932b51c97e389e7c7bf007664e0d81f913a3250
DEVISRIARJUN <devisriarjun268@gmail.com>
2026-06-18 14:49:45 +0530

    hello

---
+++ cel_to_fah.py
@@ -0,0 +1,3 @@
+# c = [23,22,26,10,15,20]
+# p = sorted(filter(lambda x : x >60 and x < 120 , map(lambda x : int((x * 9 / 5) + 32),c)),key = lambda x : x % 4)
+# print(p)
---
+++ pdf2.py
@@ -0,0 +1,8 @@
+# from functools import reduce
+# list = [10,20,30,40,50]
+# result = reduce(lambda x,y : x if x > y else y , list , 10)
+# print(result)
+
+
+
+
