3691ea4019c232a023d988fdb72aff6578cba6ab
DEVISRIARJUN <devisriarjun268@gmail.com>
2026-07-24 12:21:08 +0530

    hloo

--- dunder_magic_methods/pdf2.py
+++
@@ -1,8 +0,0 @@
-# from functools import reduce
-# list = [10,20,30,40,50]
-# result = reduce(lambda x,y : x if x > y else y , list , 10)
-# print(result)
-
-
-
-
--- dunder_magic_methods/sorted.py
+++
@@ -1,3 +0,0 @@
-l = [1,3,5,2,4,6,21,20,2]
-k = sorted(l,key= lambda x:x%3)
-print(k)
